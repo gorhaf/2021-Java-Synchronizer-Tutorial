3844756291f4b5ad8603e67a3632104be46a3fd9
Gorhaf <gorhaf@163.com>
2021-08-10 14:53:04 +0800

    04：如何使用交换器 Exchanger？
    2021版Java同步器教程04：如何使用交换器 Exchanger？

---
+++ part04/Main.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * @author 【B站】人人都是程序员
+ * @author 【掘金】人人都是程序员
+ * @author 【CSDN】人人都是程序员
+ * @author 【今日头条】人人都是程序员
+ * @author 【官方网站】www.gorhaf.com
+ * @author 【微信公众号】gorhaf
+ * 欢迎大家扫描下方二维码关注我们
+ * █▀▀▀▀▀▀▀██▀██████▀▀▀▀██▀▀▀▀▀▀▀█
+ * █ █▀▀▀█ █▄ ▀ ▄▄█▄█▄█▀██ █▀▀▀█ █
+ * █ █   █ █▄▄▀▄▀█▄▄ ██▀ █ █   █ █
+ * █ ▀▀▀▀▀ █ █ █▀▄▀▄ █ █ █ ▀▀▀▀▀ █
+ * █▀▀▀▀▀█▀▀▀▀▀█ █▀▄▀▀█ ▄▀█▀█▀█▀██
+ * █▄▄▄ ██▀█  █▀▀ ▀  ▄ ▄▀▄   ███▄█
+ * ██  ▀▀█▀ ▄ ▄ █▀▄▄▄▀▄▀▄ ▄██▀▀▄██
+ * █ █▄▄  ▀ █ ▄█▄▄▄▀▄▀█▄ ▄ ▄ █ █▄█
+ * █  ▄██▄▀██▄ █ ▀▄▀ ▀▀ ▄▄▄▀██▀▄██
+ * █ █▀▀█▀▀█▀ █▀▀█▄▀▀█▀▄ ▀▄    █▄█
+ * █ █▀▀██▀▀█▀  █▀▀  ██▄▀▀▀▀▀█▀ ▀█
+ * █▀▀▀▀▀▀▀█ ▄██▄▄▀▀▀▄▄▀ █▀█   ▄▄█
+ * █ █▀▀▀█ █▀ ▄█ █▀ ▄▀▄▄ ▀▀▀  ▀▀ █
+ * █ █   █ █ ▄▀▀▀█▄ ▄▀▄▀ █▀▀▀ ▀ ▄█
+ * █ ▀▀▀▀▀ █ ▀█ ▀█▄  █▄█▄▄█  ▄▀▄██
+ * ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // 交换器
+        Exchanger<String> exchanger = new Exchanger<>();
+        // 线程A
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String A = "苹果";
+                    // 与另一个线程进行交换，并得到另一个线程的数据
+                    String B = exchanger.exchange(A);
+                    System.out.println("A 交换过程：" + A + " ---> " + B);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        // 线程B
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String B = "香蕉";
+                    // 与另一个线程进行交换，并得到另一个线程的数据
+                    String A = exchanger.exchange(B);
+                    System.out.println("B 交换过程：" + B + " ---> " + A);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
